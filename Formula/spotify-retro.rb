7257ca818ef9b52d300d3eed5d22644329d4cc8e
lineville <lineville@github.com>
2022-08-24 09:17:39 -0700

    Bump to 0.2.0

--- Formula/spotify-retro.rb
+++ Formula/spotify-retro.rb
@@ -2,8 +2,8 @@ class SpotifyRetro < Formula
   desc 'Cross-platform Spotify Playlist Generator in Rust'
   homepage 'https://github.com/lineville/spotify-retro'
   url 'https://github.com/lineville/spotify-retro/releases/latest/download/spotify-retro-mac.tar.gz'
-  sha256 'f8518304f45d84a61e6c0f17565e4320b0c1c6ad183abd710b3f8ea425231892'
-  version '0.1.9'
+  sha256 'e1f8da5cd511490479a54119a74bdf38838a635a0cd2a5c5e552f295faed6639'
+  version '0.2.0'
 
   def install
     bin.install 'spotify-retro'
